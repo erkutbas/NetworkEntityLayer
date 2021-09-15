2f3993999cf3a438b36416c4a7ad2fbe4b0ae4ea
erkutbas <ebas1@binghamton.edu>
2021-09-15 21:31:14 +0300

    ServerResponse Updated

--- Sources/NetworkEntityLayer/Modules/ServerResponse.swift
+++ Sources/NetworkEntityLayer/Modules/ServerResponse.swift
@@ -9,11 +9,12 @@ import Foundation
 
 public struct ServerResponse: Codable, Error {
     public let message: String?
-    public let errorCode: Int?
+    public let code: String?
+    public let status: String?
 
-    public init(message: String? = nil, errorCode: Int? = nil) {
-        self.errorCode = errorCode
+    public init(message: String? = nil, code: String? = nil, status: String? = nil) {
+        self.code = code
         self.message = message
-        
+        self.status = status
     }
 }
